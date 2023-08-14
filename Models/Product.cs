7f686fd4247bb2a13d2c5c1946b4ff06f9986a54
muradazimzada <murad.azimzada.dev@gmail.com>
2023-08-14 10:34:10 +0400

    Add project files.

---
+++ Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DockerizedWebAPI.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+    }
+}
